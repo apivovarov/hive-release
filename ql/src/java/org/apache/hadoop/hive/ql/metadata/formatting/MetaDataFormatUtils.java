158a131dea28282a31e2c41d1bb30ede51048dba
Jason Dere <jdere@hortonworks.com>
2013-09-19 19:56:21 -0700

    Fix compilation error introduced due to merge of hive-0.12 and HIVE-4545

--- ql/src/java/org/apache/hadoop/hive/ql/metadata/formatting/MetaDataFormatUtils.java
+++ ql/src/java/org/apache/hadoop/hive/ql/metadata/formatting/MetaDataFormatUtils.java
@@ -25,6 +25,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.hadoop.hive.conf.HiveConf;
