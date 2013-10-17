6c8097c95723bdff55c5ab516e012e461e2ec7e8
vikram <vikram@hortonworks.com>
2013-10-17 13:20:47 -0700

    HIVE-4689 port

--- ql/src/java/org/apache/hadoop/hive/ql/exec/JoinOperator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/JoinOperator.java
@@ -94,7 +94,7 @@ public class JoinOperator extends CommonJoinOperator<JoinDesc> implements
       List keyObject = (List) soi.getStructFieldData(row, sf);
       // Are we consuming too much memory
       if (alias == numAliases - 1 && !(handleSkewJoin && skewJoinKeyContext.currBigKeyTag >= 0)) {
-        if (sz == joinEmitInterval) {
+        if (sz == joinEmitInterval && !hasFilter(alias)) {
           // The input is sorted by alias, so if we are already in the last join
           // operand,
           // we can emit some results now.
